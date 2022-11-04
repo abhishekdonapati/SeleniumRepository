a30b10974f6b28c2355bd3bd0529c269e9e82b04
abhishekdonapati <116938966+abhishekdonapati@users.noreply.github.com>
2022-11-04 11:12:55 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,36 @@
+package Demo;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Login {
+	
+	@Test
+	public void APLogin() throws InterruptedException
+	{
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\AbhishekReddyDonapat\\OneDrive - Hallmark Health Care Solutions, Inc\\Desktop\\Selenium Web\\WebDriver\\chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
+	
+	String baseUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+	driver.get(baseUrl);
+	driver.findElement(By.id("email")).sendKeys("hgdyrtyggy@gmail.com");
+	driver.findElement(By.id("passwd")).sendKeys("Naveen@123");
+	
+	driver.findElement(By.id("SubmitLogin")).click();
+	
+	if(driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a/span")).isDisplayed()) {
+		System.out.println("The confirmation message says- "+driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a/span")).getText());
+	}
+			else
+		{
+		driver.navigate().back();
+	}
+
+	
+	driver.close();
+}
+}
